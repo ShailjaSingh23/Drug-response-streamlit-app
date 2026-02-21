17210dfc49c1c00b927a3606f6fb6a3ed1f52f3c
ShailjaSingh23 <shailjasingh0304@gmail.com>
2026-02-21 15:07:43 +0530

    Update SVM_model.py

--- SVM_model.py
+++ SVM_model.py
@@ -8,13 +8,13 @@ model=joblib.load('Drug Response.pkl')
 st.title('Drug Response System')
 
 Dosage = st.number_input('Drug Dosage')
-Systolic Blood Pressure = st.number_input('Systolic Blood Pressure')
-Heart Rate = st.number_input('Heart Rate')
-Liver Toxicity Index = st.number_input('Liver Toxicity Index')
-Blood Glucose Level = st.number_input('Blood Glucose Level')
+Systolic_bp = st.number_input('Systolic Blood Pressure')
+Heart_rate = st.number_input('Heart Rate')
+Liver_TI = st.number_input('Liver Toxicity Index')
+Blood_GL = st.number_input('Blood Glucose Level')
 
 if st.button('Predict'):
-    data=np.array([[Dosage,Systolic Blood Pressure,Heart Rate,Liver Toxicity Index,Blood Glucose Level]])
+    data=np.array([[Dosage,Systolic_bp,Heart_rate,Liver_TI,Blood_GL]])
     result=model.predict(data)
 
     if result[0]==1:
